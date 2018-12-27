8a75ede339a5ba90318c6ebea264ba232531d340
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-12-27 14:33:11 +0100

    exemple

---
+++ php-template/exemple.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Exemple</title>
+    </head>
+    <body>
+        <?php 
+          echo "<p>C'est un script PHP!</p>\n"; 
+        ?>
+    </body>
+</html>
--- php-template/run.sh
+++ php-template/run.sh
@@ -1,11 +1,5 @@
 
 echo "TECHIO> redirect-streams 'Output'"
-php "header.php" $1".html" > "output.html"
+php "exemple.php" 
 
-
-#cat "output.html"
-#ls ../target -al
-#ls ../workspace -al
-
-php Test.php $1".html" $2
 echo "TECHIO> open -s /project/target/ output.html"
