03852322d88bfd8e3745e4ade5da658eb54d9b36
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-01-01 13:06:43 +0100

    updt2

--- php-template/exemple1.php
+++ php-template/exemple1.php
@@ -7,9 +7,9 @@
 		<h1>Un page avec la date courante</h1>
         <p>Nous sommes actuellement le
 		<?php 
-           echo date("j/n/Y") . " il est " . date("G:i:s");
+           echo date("d/m/Y") . " il est " . date("G:i:s");
         ?>
 		</p>
-		<p>La date n'évolue pas seule. Elle ne change que chaque fois que la page est rechargée</p>
+		<p>La date n'évolue pas seule. Elle ne change que chaque fois que la page est rechargée. <em>Attention: la date affichée est la date sur le serveur.</em></p>
     </body>
 </html>
